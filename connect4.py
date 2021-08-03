842148038f15493902e8ee781cf6074151c0ecf1
Kamran Choudhry <theblizzzzard@gmail.com>
2021-08-03 00:58:09 -0400

    nice

--- connect4.py
+++ connect4.py
@@ -1,15 +1,15 @@
-import sys
+import random
 import enum
 
 class Empty(enum.Enum):
-    EMPTY_SPACE = 'EMPTY'
+    EMPTY_SPACE = ''
 
 class Player(enum.Enum):
-    RED_PLAYER = 'RED'
-    YELLOW_PLAYER = 'YELLOW'
+    RANDOM_PLAYER = 'R'
+    HUMAN_PLAYER = 'H'
 
 class Connect4():
-    def __init__(self, n=6, m=7, default_player=Player.YELLOW_PLAYER):
+    def __init__(self, n=6, m=7, default_player=Player.HUMAN_PLAYER):
         self.n = n
         self.m = m
         self.board = self.create_board()
@@ -18,7 +18,7 @@ class Connect4():
         
     def print_board(self):
         for index, row in enumerate(self.board, start=1):
-            print(index, self.board[row])
+            print(f"{index} {[x.value for x in self.board[row]]}")
 
     def create_board(self):
         return {
@@ -36,12 +36,12 @@ class Connect4():
                     ):
                     return True
         # iterate over all cols
-        for row_index in range(self.n):
-            for col_index in range(self.m-4):
-                if (self.board[col_index][row_index].value == self.current_player.value and \
-                    self.board[col_index+1][row_index].value == self.current_player.value and \
-                    self.board[col_index+2][row_index].value == self.current_player.value and \
-                    self.board[col_index+3][row_index].value == self.current_player.value
+        for row_index in range(self.n-4):
+            for col_index in range(self.m):
+                if (self.board[row_index+1][col_index].value == self.current_player.value and \
+                    self.board[row_index+1][col_index].value == self.current_player.value and \
+                    self.board[row_index+2][col_index].value == self.current_player.value and \
+                    self.board[row_index+3][col_index].value == self.current_player.value
                     ):
                     return True
         # iterate over downwards diagonals (L -> R)
@@ -67,7 +67,7 @@ class Connect4():
     def request_move(self):
         column_entry = ''
         while (not column_entry.isnumeric() or not (1 <= int(column_entry) <= 7)):
-            column_entry = input("Please select a column between 1 and 7")
+            column_entry = input("Please select a column between 1 and 7: ")
         return int(column_entry)
     
     def update_board(self, column_entry):
@@ -80,27 +80,32 @@ class Connect4():
         self.board[row_of_interest-1][column_entry-1] = self.current_player
 
     def switch_players(self):
-        if (self.current_player.value == Player.RED_PLAYER.value):
-            self.current_player = Player.YELLOW_PLAYER
-        elif (self.current_player.value == Player.YELLOW_PLAYER.value):
-            self.current_player = Player.RED_PLAYER    
+        if (self.current_player.value == Player.RANDOM_PLAYER.value):
+            self.current_player = Player.HUMAN_PLAYER
+        elif (self.current_player.value == Player.HUMAN_PLAYER.value):
+            self.current_player = Player.RANDOM_PLAYER    
 
-    def game_engine(self):
+    def game_random_agent(self):
         print('Welcome to Connect4 - Practice Problem Edition!')
         while (not self.game_exit):
             print(f"Current player: {self.current_player}")
+            print('~'*30)
             self.print_board()
-            column_entry = self.request_move()
+            print('~'*30)
+            if (self.current_player.value == Player.RANDOM_PLAYER.value):
+                column_entry = random.randint(0, self.m)
+            else:
+                column_entry = self.request_move()
             self.update_board(column_entry)
             if (self.verify_winner()):
-                print('~'*25)
+                print('~'*30)
                 self.print_board()
+                print('~'*30)
                 print(f"Winner: {self.current_player}")
+                print('~'*30)
                 self.game_exit = True
-            else:
-                self.switch_players()
+            self.switch_players()
 
 if __name__ == '__main__':
     obj = Connect4()
-    obj.game_engine()
-    sys.exit()
+    obj.game_random_agent()
