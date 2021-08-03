73dfb2ebf3f0245d24c494f4bff7acd90b120538
Kamran Choudhry <theblizzzzard@gmail.com>
2021-08-02 22:12:13 -0400

    nice

--- connect4.py
+++ connect4.py
@@ -1,73 +1,109 @@
 from enum import Enum
 
+class Empty(Enum):
+    EMPTY_SPACE = 'EMPTY'
+
 class Player(Enum):
     RED_PLAYER = 'RED'
     YELLOW_PLAYER = 'YELLOW'
 
 class Connect4():
-    def __init__(self, n=6, m=7, default_player=Player.RED_PLAYER):
+    def __init__(self, n=6, m=7, default_player=Player.YELLOW_PLAYER):
         self.n = n
         self.m = m
         self.board = self.create_board()
-        self.default_player = default_player
+        self.current_player = default_player
+        self.game_exit = False
         
     def print_board(self):
-        print(self.board)
+        # print(self.board)
+        for k in self.board:
+            print(k, self.board[k])
 
     def create_board(self):
-        board = {i : ['EMPTY' for _ in range(self.m)] for i in range(self.n)}
-        # board[5] = [Player.RED_PLAYER, Player.RED_PLAYER, Player.RED_PLAYER, Player.RED_PLAYER, -1, -1 ,-1]
+        board = {i : [Empty.EMPTY_SPACE for _ in range(self.m)] for i in range(self.n)}
+        # board[5] = [Player.RED_PLAYER, Player.RED_PLAYER, Player.RED_PLAYER, Player.RED_PLAYER, Empty.EMPTY_SPACE, Empty.EMPTY_SPACE, Empty.EMPTY_SPACE]
         return board
 
-    def getRows(self):
-        return self.board[0], self.board[1], self.board[2], self.board[3], self.board[4], self.board[5]
-
-    def verify_winner(self, winner):    
+    def verify_winner(self):    
         # iterate over all rows
-        for row_index in self.board:
-            row = self.board[row_index]
-            i = 0
-            for j in range(3, self.m):
-                if (all(isinstance(entry, type(winner)) for entry in row[i:j+1])):
+        for row_index in range(self.n):
+            for col_index in range(self.m-4):
+                if (self.board[row_index][col_index].value == self.current_player.value and \
+                    self.board[row_index][col_index+1].value == self.current_player.value and \
+                    self.board[row_index][col_index+2].value == self.current_player.value and \
+                    self.board[row_index][col_index+3].value == self.current_player.value
+                    ):
                     return True
-                i += 1
         # iterate over all cols
-        row1, row2, row3, row4, row5, row6 = self.getRows()
-        for col in zip(row1, row2, row3, row4, row5, row6):
-            col = list(col)
-            i = 0
-            for j in range(3, self.n):
-                if (all(isinstance(entry, type(int)) for entry in col[i:j+1])):
+        for row_index in range(self.n):
+            for col_index in range(self.m-4):
+                if (self.board[col_index][row_index].value == self.current_player.value and \
+                    self.board[col_index+1][row_index].value == self.current_player.value and \
+                    self.board[col_index+2][row_index].value == self.current_player.value and \
+                    self.board[col_index+3][row_index].value == self.current_player.value
+                    ):
                     return True
-                i += 1
         # iterate over downwards diagonals (L -> R)
         for row_index in range(self.n-3):
             for col_index in range(self.m-3):
-                if (isinstance(self.board[row_index][col_index], type(winner)) & \
-                    isinstance(self.board[row_index+1][col_index+1], type(winner)) & \
-                    isinstance(self.board[row_index+2][col_index+2], type(winner)) & \
-                    isinstance(self.board[row_index+3][col_index+3], type(winner))):
+                if (self.board[row_index][col_index].value == self.current_player.value and \
+                    self.board[row_index+1][col_index+1].value == self.current_player.value and \
+                    self.board[row_index+2][col_index+2].value == self.current_player.value and \
+                    self.board[row_index+3][col_index+3].value == self.current_player.value):
                     return True
         # iterate over upwards diagonals (L -> R)
         for row_index in range(self.n-3):
             for col_index in range(3, self.m):
-                if (isinstance(self.board[row_index][col_index], type(winner)) & \
-                    isinstance(self.board[row_index-1][col_index+1], type(winner)) & \
-                    isinstance(self.board[row_index-2][col_index+2], type(winner)) & \
-                    isinstance(self.board[row_index-4][col_index+4], type(winner))):
+                if (self.board[row_index][col_index].value == self.current_player.value and \
+                    self.board[row_index+1][col_index-1].value == self.current_player.value and \
+                    self.board[row_index+2][col_index-2].value == self.current_player.value and \
+                    self.board[row_index+3][col_index-3].value == self.current_player.value):
                     return True
         return False
     
-    def update_board(self):
-        n = input("Please select a column between 1 and 7")
-        if (not (n.isnumeric() and 1 <= int(n) <= 7)):
-            self.update_board()
+    def request_move(self):
+        column_entry = input("Please select a column between 1 and 7")
+        if (not (column_entry.isnumeric() and 1 <= int(column_entry) <= 7)):
+            self.request_move()
+        return int(column_entry)
+    
+    def update_board(self, column_entry):
+        row_of_interest = 0
+        for row in self.board:
+            possible_placement = self.board[row][column_entry-1]
+            if (possible_placement.value == Empty.EMPTY_SPACE.value):
+                row_of_interest += 1
+                continue
+            else:
+                break
+        
+        print(f"modifying {row_of_interest-1, column_entry-1}")
+        self.board[row_of_interest-1][column_entry-1] = self.current_player
+        print('done')
+
+    def switch_players(self):
+        if (self.current_player.value == Player.RED_PLAYER.value):
+            self.current_player = Player.YELLOW_PLAYER
+        elif (self.current_player.value == Player.YELLOW_PLAYER.value):
+            self.current_player = Player.RED_PLAYER    
 
     def game_engine(self):
-        print(f"Starting player: {self.default_player}")
-        self.update_board()
-
+        print('Welcome to Connect4 - Practice Problem Edition!')
+        while (not self.game_exit):
+            print(f"Current player: {self.current_player}")
+            self.print_board()
+            column_entry = self.request_move()
+            self.update_board(column_entry)
+            if (self.verify_winner()):
+                print('~'*25)
+                self.print_board()
+                print(f"Winner: {self.current_player}")
+                self.game_exit = True
+            else:
+                self.switch_players()
 
 if __name__ == '__main__':
     obj = Connect4()
     obj.game_engine()
+    # print(obj.verify_winner(Player.RED_PLAYER))
