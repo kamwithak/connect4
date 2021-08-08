c0a9c14cc4cecfe8442e503b641d77d70a53dccd
Kamran Choudhry <theblizzzzard@gmail.com>
2021-08-08 16:38:19 -0400

    nice

--- test.py
+++ test.py
@@ -4,19 +4,17 @@ class BinaryTreeNode(object):
         self.right = right
         self.val = val
 
-def inOrderTraversal(self, root=None):
+def inOrderTraversal(root=None):
 
     if (not root):
         return
 
-    self.inOrderTraversal(root.left)
+    inOrderTraversal(root.left)
     print(root.val)
-    self.inOrderTraversal(root.right)
-
+    inOrderTraversal(root.right)
 
 if __name__ == '__main__':
     
-
     root = BinaryTreeNode(
         left=BinaryTreeNode(val=1),
         right=BinaryTreeNode(val=3),
